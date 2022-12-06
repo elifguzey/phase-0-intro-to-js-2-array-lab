d3d99255d0a12b42967be7cfef0216c5306ff33c
Elif Guzey <elifguzey3@gmail.com>
2022-12-06 16:40:03 -0500

    commit tree 401290fbc1b015a18b4dc7851f3d112a3c57045f

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+    return cats
+}
+function destructivelyPrependCat(name) {
+    cats.unshift (name)
+    return cats
+}
+function destructivelyRemoveLastCat (name) {
+  cats.pop ("Garfield")
+    return cats
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift ("Milo")
+    return cats
+}
+
+function appendCat(name) {
+    const newCat= [...cats]
+    newCat.push (name)
+    return newCat
+}
+function prependCat (name) {
+    const newCat= [...cats]
+    newCat.unshift (name)
+    return newCat
+}
+function removeLastCat (name) {
+    const copyOfCats = cats.slice();
+    copyOfCats.pop()
+    return copyOfCats
+}
+function removeFirstCat (name) {
+    const copyOfCats = [...cats];
+    copyOfCats.shift()
+    return copyOfCats
+}
